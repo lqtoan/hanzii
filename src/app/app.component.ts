16862dd31ce2ef608376fd211da3e112d840b22f
lqtoan <lqtoan37@gmail.com>
2023-03-28 03:16:39 +0700

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hanzii-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'hanzii';
+}
